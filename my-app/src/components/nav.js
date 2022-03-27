310099d1cb1be01a452c13d02ba539be8023c9bf
Shlok Mehra <shlokmehra@rocketmail.com>
2022-03-27 20:45:11 +0530

    Feat : Added cart navigation to cart logo in the nav component

--- my-app/src/components/nav.js
+++ my-app/src/components/nav.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { useUser } from '../context/user-context'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate } from 'react-router-dom'
 
 const Nav = () => {
-    const {state} = useUser()
+    const {state,showLoginAlert,dispatch} = useUser()
+    const navigate = useNavigate()
     //console.log("Nav state",state)
     const sumOfItems = (sum,curr) => sum + curr.qty
     const numberOfCartItems = state.cart.reduce(sumOfItems,0)
+    const handleClick = () => {
+        if(!state.encodedToken){
+            dispatch({type : "ALERT"})
+            setTimeout(() => dispatch({type : "ALERT"}),1000)
+        }
+        else{
+            navigate('/cart')
+        }
+
+    }
     return(
         <nav className = "navigation flex gap-xl align-center full-width">
             <div className="nav-brand">Water-Commerce</div>
@@ -16,7 +27,7 @@ const Nav = () => {
             <div className = "flex gap-xl flex-center grow-1" >
                 <Link to = '/login'><button className="btn btn-secondary">Login</button></Link>
                 <i className="material-icons cursor-pointer" >favorite_border</i>
-                <div class = "icon badge">
+                <div class = "icon badge" onClick = {handleClick} style = {{cursor : "pointer"}}>
 	                <span class="material-icons md-40">shopping_cart</span>
 	                <span class = "number top-right">{numberOfCartItems}</span>
                 </div>
