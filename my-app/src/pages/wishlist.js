8bb9598abb33d2805d40f41f6cb3367c83927bdb
Shlok Mehra <shlokmehra@rocketmail.com>
2022-04-01 12:55:40 +0530

    Updated wishlist.js
    Added no items in wishlist message

--- my-app/src/pages/wishlist.js
+++ my-app/src/pages/wishlist.js
@@ -6,14 +6,15 @@ import { useUser } from '../context/user-context'
 import { Link } from 'react-router-dom'
 
 const WishlistPage = () => {
-    const {state,dispatch} = useUser()
-    const renderProductCards = () => state.wishlist.map(({_id,title,author,price,category,rating}) => <ProductCard title = {title} id = {_id} author = {author} price = {price} key = {_id} category = {category} rating = {rating} /> ) 
+    const {state : {wishlist}} = useUser()
+    const renderProductCards = () => wishlist.map(({_id,title,author,price,category,rating}) => <ProductCard title = {title} id = {_id} author = {author} price = {price} key = {_id} category = {category} rating = {rating} /> ) 
     return(
         <div>
             <Nav />
             <Link to = "/products" className='router-link padding-m'>Back to Products</Link>
             <div class = "primary-text bold text-m center-text m3-top">My Wishlist</div>
             <div class="container center-block m5-top flex gap-m space-around" style = {{position: "relative"}}>
+                {wishlist.lenght === 0 ? <div className="text-m center-text m5-top bold">No Items in Wishlist</div> : null}
                 {renderProductCards()}
             </div>
             <Footer />
