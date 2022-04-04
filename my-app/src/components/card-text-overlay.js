b4584fb548254da319873c16054819a666c49c78
Shlok Mehra <shlokmehra@rocketmail.com>
2022-04-04 13:07:40 +0530

    Feat : Added Filter to Homepage

--- my-app/src/components/card-text-overlay.js
+++ my-app/src/components/card-text-overlay.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useProductListFunction } from '../context/product-listing-context';
+import { useNavigate } from 'react-router-dom'
 
 const CardWithOverlayText = ({category}) => {
+
+    const {dispatch} = useProductListFunction()
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        dispatch({type : "CATEGORY_RESET"})
+        dispatch({type : "CATEGORY" , payload : category.toLowerCase()})
+        navigate('/products')
+    }
+
     return(
-        <div className="card-container">
-            <a href="product-page.html">
-                <div className="img-container badge flex flex-center">
-                    <img
+        <div className="card-container cursor-pointer" onClick = {handleClick}>
+            <div className="img-container badge flex flex-center">
+                <img
                     src="https://images.unsplash.com/photo-1643313947128-849e4da4fd5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2142&q=80"
                     alt = "card-image"
-                    />
-                    <span className="badge-icon">New</span>
-                    <span className="overlay-text">{category}</span>
-                </div>
-            </a>  
+                />
+                <span className="badge-icon">New</span>
+                <span className="overlay-text">{category}</span>
+            </div>
         </div>
     )
 }
